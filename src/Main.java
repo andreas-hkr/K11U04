c2006c2ae40a3ac05f6c6d99e86ad1427a16e2ef
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-18 10:19:51 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // Skapa en metod som fungerar som System.arraycopy
+        //  - Array att kopiera från
+        //  - Startindex i "frånarrayen"
+        //  - Array att kopiera till
+        //  - Startindex i "tillarrayen"
+        //  - Antal komponenter/element att kopiera
+
+
+    }
+}
