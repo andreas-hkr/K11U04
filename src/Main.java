26e25e845aa9874fa542fd12f032b2e159433018
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-18 10:30:54 +0100

    Solved

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,12 @@
+import java.util.Arrays;
+
 public class Main {
+    static void arraycopy(double[] from, int fromIndex, double[] to, int toIndex, int count) {
+        for (int i=0, f=fromIndex, t=toIndex; i < count; i++, f++, t++) {
+            to[t] = from[f];
+        }
+    }
+
     public static void main(String[] args) {
         // Skapa en metod som fungerar som System.arraycopy
         //  - Array att kopiera från
@@ -7,6 +15,12 @@ public class Main {
         //  - Startindex i "tillarrayen"
         //  - Antal komponenter/element att kopiera
 
+        double[] from = {1, 2, 3, 4, 5};
+        double[] to = {9, 8, 7, 6, 5};
 
+        arraycopy(from,1,to,2,2);
+
+        System.out.println(Arrays.toString(from));
+        System.out.println(Arrays.toString(to));
     }
 }
